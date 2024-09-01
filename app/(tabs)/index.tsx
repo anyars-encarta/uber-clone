bdc91142db470b9032a7f062946d531c9fde6577
anyars-encarta <anyarsencarta@gmail.com>
2024-09-01 17:07:47 +0000

    Install expo eslint

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 export default function HomeScreen() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <Text className='text-red-500'>Uber Clone</Text>
+      <Text>Uber Clone!</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
