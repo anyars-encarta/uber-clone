81b51dc42c832f7fc1fda3208d4a3dd2cb1da74f
anyars-encarta <anyarsencarta@gmail.com>
2024-09-01 10:38:49 +0000

    Correct styling config

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,11 @@
-import { View, Text, StatusBar } from 'react-native';
+import { Text, StatusBar } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text className='text-red-500'>Uber Clone</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
-// tailwind.config.js
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
