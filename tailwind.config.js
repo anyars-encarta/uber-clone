b4b5e5c6f1ee92cbec56c3ee61fe822f384ec540
anyars-encarta <anyarsencarta@gmail.com>
2024-09-01 09:13:31 +0000

    Configure tailwind.config.js and babel.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+
 module.exports = {
   content: [],
+  content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
