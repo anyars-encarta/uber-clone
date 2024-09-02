7a6096a66c38ae3f4ea70f89b88026ef70330da1
anyars-encarta <anyarsencarta@gmail.com>
2024-09-02 11:03:51 +0000

    Create route groups

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const SignUp = () => {
+    return (
+        <SafeAreaView>
+            <Text>SignUp</Text>
+        </SafeAreaView>
+    )
+}
+
+export default SignUp
